d9a37a7c124cf16780b1df6ffe12041c591599b5
nowafalcon <nowafalcon@gmail.com>
2021-05-16 21:56:44 +0300

    Lesson6-started

--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -6,7 +6,9 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-from .settings import BOT_NAME
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
+from settings import BOT_NAME
 from pymongo import MongoClient
 
 
@@ -22,4 +24,15 @@ class GbMongoPipeline:
 
     def process_item(self, item, spider):
         self.db[spider.name].insert_one(item)
+        return item
+
+
+class GbImageDownloadPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for url in item.get("photos", []):
+            yield Request(url)
+
+    def item_completed(self, results, item, info):
+        if item.get("photos"):
+            item["photos"] = [itm[1] for itm in results]
         return item
